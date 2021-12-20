3141ee6eb5ff72845a05add48fc33cda0ef857c1
Pennervomland <marcdommroese@googlemail.com>
2021-12-20 13:25:05 +0100

    Karte Änderungen
    - Manakosten für Karte (nur graphisch)
    - Mana für Spieler (nur graphisch)
    - Karte wird jetzt in die Mitte gesetzt auf linksklick wenn Maus über karte und zurückgesetzt durch rechtsklick
    - Bisher noch keine richtige "Hand" des Spielers deswegen muss das zurücksetzen dementsprechend bearbeitet werden wenn "Hand" implementiert wird

--- Card.gd
+++ Card.gd
@@ -9,6 +9,7 @@ export var mana_regeneration = 1
 
 var is_mouse_on_card: bool
 var is_card_in_use: bool
+var is_card_in_focus: bool
 
 var mouse_pos = get_global_mouse_position()
 
@@ -17,16 +18,24 @@ onready var start_pos = position
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	is_mouse_on_card = false
+	is_card_in_use = false
+	is_card_in_focus = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
 	#Has to be in game script later and improve!
-	if(Input.is_action_just_pressed("LeftMB")== true):
-		print(get_viewport_rect())
-	if(Input.is_action_just_released("LeftMB") == true):
+	if(Input.is_action_just_pressed("LeftMB")== true && is_mouse_on_card==true):
+		is_card_in_use = true
+		is_card_in_focus = false
+	if(Input.is_action_just_pressed("RightMB") == true && is_card_in_use == true):
+		is_card_in_use = false
+		scale = Vector2(0.5,0.5)
 		position = start_pos
+	if(is_card_in_use == true):
+		scale = Vector2(1,1)
+		position = Vector2(512,220)
 
 func trigger_effect():
 	pass
@@ -34,10 +43,14 @@ func trigger_effect():
 
 func _on_Area2D_mouse_entered():
 	is_mouse_on_card = true
-	scale.x = 0.75
-	scale.y = 0.75
+	if(is_card_in_use == false):
+		scale.x = 0.75
+		scale.y = 0.75
+		is_card_in_focus = true
 
 func _on_Area2D_mouse_exited():
-	scale.x = 0.5
-	scale.y = 0.5
+	if(is_card_in_use == false):
+		scale.x = 0.5
+		scale.y = 0.5
 	is_mouse_on_card = false
+	is_card_in_focus = false
