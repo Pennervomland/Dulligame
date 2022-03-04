03a34dfd2ccefff7e2728af1c18c6f021db097fd
Pennervomland <marcdommroese@googlemail.com>
2022-03-04 20:10:29 +0100

    Bot finished.... probably not rly but it is!11!

--- Scripts/BotCyberpenner.gd
+++ Scripts/BotCyberpenner.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-
 onready var game = get_tree().root.get_child(1)
 onready var chat_label = $Communication/Chat/ChatLabel
 onready var chat_timer = $Communication/ChatTimer
@@ -16,7 +15,7 @@ var start_hp
 var is_player1
 var player_name = "Penner"
 
-export var hp = 100
+export var hp = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -24,6 +23,7 @@ func _ready():
 
 func begin_turn():
 	attack_timer.start()
+	bot_ui.get_child(0).visible = false
 
 func put_salt_in_wound():
 	chat_timer.start()
@@ -40,7 +40,8 @@ func start_end_turn_timer():
 
 func roll_attack():
 	var rng = RandomNumberGenerator.new()
-	var number = rng.randi_range(3, 3)
+	rng.randomize()
+	var number = rng.randi_range(1, 3)
 	choose_attack(number)
 
 func choose_attack(var attack_number):
@@ -65,7 +66,7 @@ func apply_enemy_heal(var heal):
 	Global.inactive_player.apply_healing(heal)
 
 func attack_three():
-	var instance = attack_three_animation
+	var instance = attack_three_animation.instance()
 	add_child(instance)
 	instance.position.x = -700
 	attack_three_timer.start()
@@ -73,7 +74,22 @@ func attack_three():
 func apply_enemy_damage(var damage):
 	Global.inactive_player.apply_damage(damage)
 
+func hide_ui():
+	pass
 
+func apply_damage(var damage):
+	hp_bar.value -= damage
+	if hp_bar.value <= 0:
+		rotation_degrees = 180
+		attack_three_timer.paused = true
+		end_turn_timer.paused = true
+		attack_timer.paused = true
+		bot_ui.get_child(3).get_child(0).rect_position.x += 100
+		bot_ui.get_child(3).get_child(0).text = "Cyberpenner LOST"
+		chat.visible = true
+		chat.rotation_degrees = 180
+		chat.position.x += 400
+		chat_label.text = "FUUUUUUUUUUUUUUUUUCK"
 
 func _on_ChatTimer_timeout():
 	chat.visible = false
@@ -84,12 +100,14 @@ func _on_AttackTimer_timeout():
 
 
 func _on_EndTurnTimer_timeout():
+	bot_ui.get_child(0).visible = true
 	bot_ui._on_EndTurnButton_pressed()
 	end_turn_timer.stop()
 
 
 func _on_AttackThreeTimer_timeout():
-	Global.inactive_player.apply_damage(50)
+	Global.inactive_player.apply_damage(30)
 	chat_timer.start()
 	chat.visible = true
 	chat_label.text = "GET FUCKED N00B"
+	start_end_turn_timer()
